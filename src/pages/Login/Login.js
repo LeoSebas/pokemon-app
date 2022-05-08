4d5b30445415bfb6120b05658ad10d9169b60151
Leonardo Canteros <leo.sebas.065@gmail.com>
2022-05-07 21:38:25 -0300

    Estructura básica

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -5,11 +5,17 @@ import { useNavigate } from "react-router-dom"
 
 export default function Login() {
 
+    /// Usuario actual o null si no existe.
     const {user} = useContext(AuthContext)
 
+    /// Navigate para redireccionar
     const navigate = useNavigate()
 
-    useEffect(() => user ? navigate('/', {replace : true}) : undefined, [user, navigate])
+    useEffect(
+        /// Ternario para redireccionar a la pagina principal si ya inicio sesión
+
+        () => user ? navigate('/') : undefined
+        , [user, navigate])
 
     return <Container>
         <h1>Login Page</h1>
